c535a500a5ed85a149cc1a56ac63ac345d81f615
joacoox <joaquinarieldominguez@gmail.com>
2025-06-23 14:32:30 -0300

    vercel fix

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
+}
+
+bootstrap();
+
+export default server;

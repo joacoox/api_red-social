67d97c1f375346462d548eda5f52bfad9f7e828c
joacoox <joaquinarieldominguez@gmail.com>
2025-06-24 21:30:07 -0300

    vercel fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,12 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 import { AutenticacionModule } from './autenticacion/autenticacion.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PublicationModule } from './publication/publication.module';
 import { JwtService } from './servicices/jwt/jwt.service';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
     AutenticacionModule,
     UsuariosModule,
     ConfigModule.forRoot(),

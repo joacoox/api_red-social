fa981fbacbf9712456b8e78a79b12499bf8eab15
joacoox <joaquinarieldominguez@gmail.com>
2025-06-25 00:48:38 -0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONT_URL,
+    origin: process.env.FRONT_URL || 'https://app-red-social.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
